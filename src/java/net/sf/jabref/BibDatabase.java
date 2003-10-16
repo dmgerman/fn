3c8492089d46440231aeaf170c83bed016aeca30
mortenalver <mortenalver@users.sourceforge.net>
2003-10-16 20:33:32 +0000

    Adapted and added several Bibkeeper files, that make up a complete data structure for a database.
    Former-commit-id: c47ed14696aa03de7ba9e23e0fb037057aa58708

--- src/java/net/sf/jabref/BibDatabase.java
+++ src/java/net/sf/jabref/BibDatabase.java
@@ -1,4 +1,8 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/* Copyright (C) 2003 Nizar N. Batada nbatada@stanford.edu Morten O. Alver alver@boblefisk.org  All programs in this directory and subdirectories are published under the GNU General Public License as described below.  This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.  You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA  Further information about the GNU GPL is available at: http://www.gnu.org/copyleft/gpl.ja.html  */
+end_comment
+
 begin_package
 DECL|package|net.sf.jabref
 package|package
@@ -77,11 +81,38 @@ parameter_list|,
 name|BibEntry
 name|entry
 parameter_list|)
+throws|throws
+name|KeyCollisionException
 block|{
 comment|// Inserts entry into _entries, but only if there is no entry
 comment|// there already with the same id.
+if|if
+condition|(
+name|_entries
+operator|.
+name|containsKey
+argument_list|(
+name|id
+argument_list|)
+condition|)
+throw|throw
+operator|new
+name|KeyCollisionException
+argument_list|(
+literal|"ID already used in database"
+argument_list|)
+throw|;
+name|_entries
+operator|.
+name|put
+argument_list|(
+name|id
+argument_list|,
+name|entry
+argument_list|)
+expr_stmt|;
 block|}
-comment|/**      * Get an entry by its ID.      */
+comment|/**      * Get an entry by its ID. Returns null if id doesn't exist.      */
 DECL|method|getEntry (String id)
 specifier|public
 name|BibEntry
@@ -112,17 +143,17 @@ parameter_list|(
 name|String
 name|id
 parameter_list|)
-block|{      }
+block|{
+name|_entries
+operator|.
+name|remove
+argument_list|(
+name|id
+argument_list|)
+expr_stmt|;
+block|}
 comment|/**      * Remove an entry.      */
-DECL|method|removeEntry (BibEntry entry)
-specifier|public
-name|void
-name|removeEntry
-parameter_list|(
-name|BibEntry
-name|entry
-parameter_list|)
-block|{      }
+comment|/*    public void removeEntry(BibEntry entry) {      }*/
 comment|/**      * To get keys for all entries.      */
 DECL|method|getKeySet ()
 specifier|public
