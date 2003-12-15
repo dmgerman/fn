0f7f17dadbdd261d6308b4e026b9cbffea83c0d5
mortenalver <mortenalver@users.sourceforge.net>
2003-12-15 21:29:31 +0000

    Changed color of EntryTypeDialog, updated resource files, and updated EntryEditorHelp.
    Former-commit-id: 7b9f1732711fcf96c97bf89fb29f4f4e2a26c493

--- src/java/net/sf/jabref/EntryTypeDialog.java
+++ src/java/net/sf/jabref/EntryTypeDialog.java
@@ -273,6 +273,24 @@ argument_list|(
 name|this
 argument_list|)
 expr_stmt|;
+name|pan
+operator|.
+name|setBackground
+argument_list|(
+name|GUIGlobals
+operator|.
+name|lightGray
+argument_list|)
+expr_stmt|;
+name|lower
+operator|.
+name|setBackground
+argument_list|(
+name|GUIGlobals
+operator|.
+name|lightGray
+argument_list|)
+expr_stmt|;
 comment|// Make ESC close dialog, equivalent to clicking Cancel.
 name|cancel
 operator|.
