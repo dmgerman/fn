9c2c3d3c3c153181c91473af8bc370f064573636
mortenalver <mortenalver@users.sourceforge.net>
2004-05-02 16:25:43 +0000

    Added BibTeXML export
    Former-commit-id: f47eaefd46f4d4fa80fd73ef497544635a4b5f0c

---
+++ src/java/net/sf/jabref/export/layout/format/ToLowerCase.java
@@ -0,0 +1,169 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|///////////////////////////////////////////////////////////////////////////////
+end_comment
+
+begin_comment
+comment|//  Filename: $RCSfile$
+end_comment
+
+begin_comment
+comment|//  Purpose:  Atom representation.
+end_comment
+
+begin_comment
+comment|//  Language: Java
+end_comment
+
+begin_comment
+comment|//  Compiler: JDK 1.4
+end_comment
+
+begin_comment
+comment|//  Authors:  Egon Willighagen
+end_comment
+
+begin_comment
+comment|//  Version:  $Revision$
+end_comment
+
+begin_comment
+comment|//            $Date$
+end_comment
+
+begin_comment
+comment|//            $Author$
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|//  Copyright (c) Egon Willighagen
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|//  This program is free software; you can redistribute it and/or modify
+end_comment
+
+begin_comment
+comment|//  it under the terms of the GNU General Public License as published by
+end_comment
+
+begin_comment
+comment|//  the Free Software Foundation version 2 of the License.
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|//  This program is distributed in the hope that it will be useful,
+end_comment
+
+begin_comment
+comment|//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+end_comment
+
+begin_comment
+comment|//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+end_comment
+
+begin_comment
+comment|//  GNU General Public License for more details.
+end_comment
+
+begin_comment
+comment|///////////////////////////////////////////////////////////////////////////////
+end_comment
+
+begin_package
+DECL|package|net.sf.jabref.export.layout.format
+package|package
+name|net
+operator|.
+name|sf
+operator|.
+name|jabref
+operator|.
+name|export
+operator|.
+name|layout
+operator|.
+name|format
+package|;
+end_package
+
+begin_import
+import|import
+name|net
+operator|.
+name|sf
+operator|.
+name|jabref
+operator|.
+name|export
+operator|.
+name|layout
+operator|.
+name|LayoutFormatter
+import|;
+end_import
+
+begin_comment
+comment|/**  * Remove brackets formatter.  *  * @author $author$  * @version $Revision$  */
+end_comment
+
+begin_class
+DECL|class|ToLowerCase
+specifier|public
+class|class
+name|ToLowerCase
+implements|implements
+name|LayoutFormatter
+block|{
+comment|//~ Methods ////////////////////////////////////////////////////////////////
+DECL|method|format (String fieldText)
+specifier|public
+name|String
+name|format
+parameter_list|(
+name|String
+name|fieldText
+parameter_list|)
+block|{
+name|String
+name|fieldEntry
+init|=
+name|fieldText
+operator|.
+name|toLowerCase
+argument_list|()
+decl_stmt|;
+return|return
+name|fieldEntry
+return|;
+block|}
+block|}
+end_class
+
+begin_comment
+comment|///////////////////////////////////////////////////////////////////////////////
+end_comment
+
+begin_comment
+comment|//  END OF FILE.
+end_comment
+
+begin_comment
+comment|///////////////////////////////////////////////////////////////////////////////
+end_comment
+
+end_unit
+
