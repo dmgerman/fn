128163746e1c7bb0874b2d449d1aee6a2519c72b
mortenalver <mortenalver@users.sourceforge.net>
2004-11-30 22:32:07 +0000

    Added AbstractWorker, Worker and CallBack that take advantage of the Spin library.
    Former-commit-id: 435ff01f5eff8804c48e183121cb4613188a375a

---
+++ src/java/net/sf/jabref/Worker.java
@@ -0,0 +1,33 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_package
+DECL|package|net.sf.jabref
+package|package
+name|net
+operator|.
+name|sf
+operator|.
+name|jabref
+package|;
+end_package
+
+begin_comment
+comment|/**  * ...  */
+end_comment
+
+begin_interface
+DECL|interface|Worker
+specifier|public
+interface|interface
+name|Worker
+block|{
+DECL|method|run ()
+specifier|public
+name|void
+name|run
+parameter_list|()
+function_decl|;
+block|}
+end_interface
+
+end_unit
+
