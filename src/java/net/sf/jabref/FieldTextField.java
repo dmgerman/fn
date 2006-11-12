98b5024ec8c5dfe7a6a4c43b6b429ddb6cf6df2f
coezbek <coezbek@users.sourceforge.net>
2006-11-12 01:41:55 +0000

    Corrected to fix [ 1594169 ] Entry editor: navigation between panels
    Former-commit-id: e831645a3d0e8329d83dcf040e65af59fa6a03b6

--- src/java/net/sf/jabref/FieldTextField.java
+++ src/java/net/sf/jabref/FieldTextField.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/* Copyright (C) 2003 Morten O. Alver, Nizar N. Batada  All programs in this directory and subdirectories are published under the GNU General Public License as described below.  This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.  You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA  Further information about the GNU GPL is available at: http://www.gnu.org/copyleft/gpl.ja.html  */
+comment|/*  Copyright (C) 2003 Morten O. Alver, Nizar N. Batada   All programs in this directory and  subdirectories are published under the GNU General Public License as  described below.   This program is free software; you can redistribute it and/or modify  it under the terms of the GNU General Public License as published by  the Free Software Foundation; either version 2 of the License, or (at  your option) any later version.   This program is distributed in the hope that it will be useful, but  WITHOUT ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU  General Public License for more details.   You should have received a copy of the GNU General Public License  along with this program; if not, write to the Free Software  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA   Further information about the GNU GPL is available at:  http://www.gnu.org/copyleft/gpl.ja.html   */
 end_comment
 
 begin_package
@@ -111,7 +111,8 @@ argument_list|(
 name|content
 argument_list|)
 expr_stmt|;
-comment|// Add the global focus listener, so a menu item can see if this field was focused when
+comment|// Add the global focus listener, so a menu item can see if this field
+comment|// was focused when
 comment|// an action was called.
 name|addFocusListener
 argument_list|(
@@ -152,8 +153,8 @@ operator|+
 literal|" "
 argument_list|)
 expr_stmt|;
-comment|//label = new JLabel(" "+Util.nCase(fieldName)+" ", JLabel.CENTER);
-comment|//label.setBorder(BorderFactory.createEtchedBorder());
+comment|// label = new JLabel(" "+Util.nCase(fieldName)+" ", JLabel.CENTER);
+comment|// label.setBorder(BorderFactory.createEtchedBorder());
 name|setBackground
 argument_list|(
 name|GUIGlobals
@@ -161,11 +162,11 @@ operator|.
 name|validFieldBackground
 argument_list|)
 expr_stmt|;
-comment|//label.setOpaque(true);
-comment|//if ((content != null)&& (content.length()> 0))
-comment|//label.setForeground(GUIGlobals.validFieldColor);
+comment|// label.setOpaque(true);
+comment|// if ((content != null)&& (content.length()> 0))
+comment|// label.setForeground(GUIGlobals.validFieldColor);
 comment|// At construction time, the field can never have an invalid value.
-comment|//else label.setForeground(GUIGlobals.nullFieldColor);
+comment|// else label.setForeground(GUIGlobals.nullFieldColor);
 name|FieldTextMenu
 name|popMenu
 init|=
