a6f87f442fc865ac78bf0253f1b86bd897404588
coezbek <coezbek@users.sourceforge.net>
2007-09-10 11:07:31 +0000

    Improved new Main class (thanks Ambrogio).
    Former-commit-id: c3860e601e242ba8bfb0b86ae79c67a8f4d9bd37

--- src/java/net/sf/jabref/JabRefMain.java
+++ src/java/net/sf/jabref/JabRefMain.java
@@ -35,7 +35,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * This is a class compiled under Java 1.4.2 that will start the real JabRef, if  * Java 1.5 or higher is installed.  *   * @author oezbek  *   */
+comment|/**  * This is a class compiled under Java 1.4.2 that will start the real JabRef and  * print some warnings if no Java 1.5 and higher and no JRE from Sun  * Microsystems is found.  *   * Caution: We cannot use any other class from JabRef here (for instance no  * calls to Globals.lang() are possible), since then it could not be run using  * Java 1.4.  *   * @author oezbek  *   */
 end_comment
 
 begin_class
@@ -74,58 +74,39 @@ name|javaVersion
 operator|.
 name|compareTo
 argument_list|(
-literal|"1.4"
+literal|"1.5"
 argument_list|)
-operator|<=
+operator|<
 literal|0
 condition|)
 block|{
-name|System
-operator|.
-name|out
-operator|.
-name|println
-argument_list|(
+name|String
+name|javaVersionWarning
+init|=
+literal|"\n"
+operator|+
 literal|"WARNING: You are running Java version 1.4 or lower ("
 operator|+
 name|javaVersion
 operator|+
-literal|" to be exact)."
-argument_list|)
-expr_stmt|;
+literal|" to be exact).\n"
+operator|+
+literal|"         JabRef needs at least a Java Runtime Environment 1.5 or higher.\n"
+operator|+
+literal|"         JabRef should not start properly and output an error message\n"
+operator|+
+literal|"         (probably java.lang.UnsupportedClassVersionError ... (Unsupported major.minor version 49.0)\n"
+operator|+
+literal|"         See http://jabref.sf.net/faq.php for more information.\n"
+decl_stmt|;
 name|System
 operator|.
 name|out
 operator|.
 name|println
 argument_list|(
-literal|"         JabRef needs at least a Java Runtime Environment 1.5 or higher."
+name|javaVersionWarning
 argument_list|)
-expr_stmt|;
-name|System
-operator|.
-name|out
-operator|.
-name|println
-argument_list|(
-literal|"         JabRef should not start properly and output a strange error message."
-argument_list|)
-expr_stmt|;
-name|System
-operator|.
-name|out
-operator|.
-name|println
-argument_list|(
-literal|"         See http://jabref.sf.net/faq.php for more information."
-argument_list|)
-expr_stmt|;
-name|System
-operator|.
-name|out
-operator|.
-name|println
-argument_list|()
 expr_stmt|;
 block|}
 name|String
@@ -142,13 +123,15 @@ argument_list|)
 decl_stmt|;
 if|if
 condition|(
-operator|!
 name|javaVendor
 operator|.
-name|contains
+name|indexOf
 argument_list|(
 literal|"Sun Microsystems"
 argument_list|)
+operator|==
+operator|-
+literal|1
 condition|)
 block|{
 name|System
@@ -157,48 +140,26 @@ name|out
 operator|.
 name|println
 argument_list|(
-literal|"WARNING: You are not running a Java version from Sun Microsystems."
-argument_list|)
-expr_stmt|;
-name|System
-operator|.
-name|out
-operator|.
-name|println
-argument_list|(
+literal|"\n"
+operator|+
+literal|"WARNING: You are not running a Java version from Sun Microsystems.\n"
+operator|+
 literal|"         Your java vendor is: "
 operator|+
 name|javaVendor
+operator|+
+literal|"\n"
+operator|+
+literal|"         If JabRef crashes please consider switching to a Sun Java Runtime.\n"
+operator|+
+literal|"         See http://jabref.sf.net/faq.php for more information.\n"
 argument_list|)
-expr_stmt|;
-name|System
-operator|.
-name|out
-operator|.
-name|println
-argument_list|(
-literal|"         If JabRef crashes please consider switching to a Sun Java Runtime."
-argument_list|)
-expr_stmt|;
-name|System
-operator|.
-name|out
-operator|.
-name|println
-argument_list|(
-literal|"         See http://jabref.sf.net/faq.php for more information."
-argument_list|)
-expr_stmt|;
-name|System
-operator|.
-name|out
-operator|.
-name|println
-argument_list|()
 expr_stmt|;
 block|}
 try|try
 block|{
+comment|// We need to load this class dynamically, or otherwise the Java
+comment|// runtime would crash while loading JabRefMain itself.
 name|Method
 name|method
 init|=
@@ -251,7 +212,7 @@ name|out
 operator|.
 name|println
 argument_list|(
-literal|"ERROR while starting JabRef:"
+literal|"ERROR while starting or running JabRef:\n"
 argument_list|)
 expr_stmt|;
 name|e
@@ -261,6 +222,38 @@ argument_list|()
 operator|.
 name|printStackTrace
 argument_list|()
+expr_stmt|;
+name|System
+operator|.
+name|out
+operator|.
+name|println
+argument_list|()
+expr_stmt|;
+name|System
+operator|.
+name|out
+operator|.
+name|println
+argument_list|(
+literal|"Please tell the JabRef developers about this by writing a bug report.\n"
+operator|+
+literal|"You can find our bug tracker at http://sourceforge.net/tracker/?atid=600306&group_id=92314\n"
+operator|+
+literal|"If the bug has already been reported there, please add your comments to the existing bug.\n"
+operator|+
+literal|"If the bug has not been reported yet, then we need the complete error message given above\n"
+operator|+
+literal|"and a description of what you did before the error occured.\n"
+operator|+
+literal|"In most cases we also need your JabRef version, the java version\n"
+operator|+
+literal|"(use 'java -version' for this) and the operating system you are using.\n"
+operator|+
+literal|"\n"
+operator|+
+literal|"We are sorry for the trouble and thanks for reporting problems with JabRef!\n"
+argument_list|)
 expr_stmt|;
 block|}
 catch|catch
@@ -355,6 +348,31 @@ argument_list|(
 literal|"This error should not happen."
 operator|+
 literal|" Write an email to the JabRef developers and tell them 'IllegalAccessException in JabRefMain'"
+argument_list|)
+expr_stmt|;
+block|}
+catch|catch
+parameter_list|(
+name|UnsupportedClassVersionError
+name|e
+parameter_list|)
+block|{
+name|e
+operator|.
+name|printStackTrace
+argument_list|()
+expr_stmt|;
+name|System
+operator|.
+name|out
+operator|.
+name|println
+argument_list|(
+literal|"\nThis means that your Java version ("
+operator|+
+name|javaVersion
+operator|+
+literal|") is not high enough to run JabRef.\nPlease update your Java Runtime Environment to a version 1.5 or higher.\n"
 argument_list|)
 expr_stmt|;
 block|}
