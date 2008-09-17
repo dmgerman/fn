4726d83038b26dcd9a05722098b175f3b4615500
mortenalver <mortenalver@users.sourceforge.net>
2008-09-17 17:42:14 +0000

    Moved page loading from FindFullText to the individual FullTextFinder implementations, but added static utility method loadPage() in FindFullText. Added SpringerLink implementation that supports downloading in non-logged-in mode.
    Former-commit-id: 217b2d38b7ab9f8cd3e69a506130f5910ff0fa5d

--- src/java/net/sf/jabref/external/FullTextFinder.java
+++ src/java/net/sf/jabref/external/FullTextFinder.java
@@ -22,6 +22,16 @@ name|URL
 import|;
 end_import
 
+begin_import
+import|import
+name|java
+operator|.
+name|io
+operator|.
+name|IOException
+import|;
+end_import
+
 begin_comment
 comment|/**  * This interface is used for classes that try to resolve a full-text PDF url from an article  * web page. Implementing classes should specialize on specific article sites.  *  */
 end_comment
@@ -32,7 +42,7 @@ specifier|public
 interface|interface
 name|FullTextFinder
 block|{
-comment|/**      * Report whether this FullTextFinder works for the site providing the given URL.      *      * @return true if the site is supported, false otherwise. If the site might be supported,      *   it is best to return true.      */
+comment|/**      * Report whether this FullTextFinder works for the site providing the given URL.      *      * @param url The url to check.      * @return true if the site is supported, false otherwise. If the site might be supported,      *   it is best to return true.      */
 DECL|method|supportsSite (URL url)
 specifier|public
 name|boolean
@@ -42,18 +52,17 @@ name|URL
 name|url
 parameter_list|)
 function_decl|;
-comment|/**      * Take the source HTML for an article page, and try to find the URL to the      * full text for this article.      *      * @param url The URL to the article's web page.      * @param pageSource The contents of the article's web page.      * @return The fulltext PDF URL, if found, or null if not found.      */
-DECL|method|findFullTextURL (URL url, String pageSource)
+comment|/**      * Take the source HTML for an article page, and try to find the URL to the      * full text for this article.      *      * @param url The URL to the article's web page.      * @return The fulltext PDF URL, if found, or null if not found.      * @throws java.io.IOException      */
+DECL|method|findFullTextURL (URL url)
 specifier|public
 name|URL
 name|findFullTextURL
 parameter_list|(
 name|URL
 name|url
-parameter_list|,
-name|String
-name|pageSource
 parameter_list|)
+throws|throws
+name|IOException
 function_decl|;
 block|}
 end_interface
