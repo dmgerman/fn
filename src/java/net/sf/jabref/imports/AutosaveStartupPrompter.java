5c8c8aff5f0468b99fdd6383c58c327df80e12a3
fdar <fdar@users.sourceforge.net>
2010-12-08 08:53:46 +0000

    Improvement in localization
    Former-commit-id: 409a869b87f97d863414ce8953609634e3077eed

--- src/java/net/sf/jabref/imports/AutosaveStartupPrompter.java
+++ src/java/net/sf/jabref/imports/AutosaveStartupPrompter.java
@@ -248,7 +248,12 @@ operator|.
 name|lang
 argument_list|(
 literal|"An autosave file was found for this database. This could indicate "
+argument_list|)
 operator|+
+name|Globals
+operator|.
+name|lang
+argument_list|(
 literal|"that JabRef didn't shut down cleanly last time the file was used."
 argument_list|)
 operator|+
