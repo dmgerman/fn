da66f9d02e33ffe261fdfb68e5c31bd6b027acc5
Morten Omholt Alver <mortenalver@gmail.com>
2012-03-26 19:29:04 +0200

    Added name of bib file to dialog header when showing warning message dialog.
    Former-commit-id: 8c5b3a564a7264c6ac5724e26cbf6eb8f848af01

--- src/java/net/sf/jabref/JabRef.java
+++ src/java/net/sf/jabref/JabRef.java
@@ -5095,7 +5095,24 @@ name|Globals
 operator|.
 name|lang
 argument_list|(
+name|Globals
+operator|.
+name|lang
+argument_list|(
 literal|"Warnings"
+argument_list|)
+operator|+
+literal|" ("
+operator|+
+name|pr
+operator|.
+name|getFile
+argument_list|()
+operator|.
+name|getName
+argument_list|()
+operator|+
+literal|")"
 argument_list|)
 argument_list|,
 name|JOptionPane
