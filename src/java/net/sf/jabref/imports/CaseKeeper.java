a14a9ab97199dd97b06cbac6e99d3eac6993ff69
Oscar Gustafsson <ogib@users.sourceforge.net>
2012-11-15 00:31:12 +0100

    Many words added to the IEEE Xplore words list and a better regular expression for replacing words
    Former-commit-id: eacbda478ffac001cf46602807545404929d5faf

--- src/java/net/sf/jabref/imports/CaseKeeper.java
+++ src/java/net/sf/jabref/imports/CaseKeeper.java
@@ -112,6 +112,7 @@ name|LengthComparator
 argument_list|()
 argument_list|)
 expr_stmt|;
+comment|// Seems like the sorting is not working
 comment|// For each word in the list
 for|for
 control|(
@@ -130,14 +131,14 @@ name|i
 operator|++
 control|)
 block|{
-comment|// Add {} if the character before is a space, -, /, or } or if it is at the start of the string but not if it is followed by a }
+comment|// Add {} if the character before is a space, -, /, (, [, or } or if it is at the start of the string but not if it is followed by a }
 name|text
 operator|=
 name|text
 operator|.
 name|replaceAll
 argument_list|(
-literal|"(^|[- /}])"
+literal|"(^|[- /\\[(}])"
 operator|+
 name|listOfWords
 index|[
