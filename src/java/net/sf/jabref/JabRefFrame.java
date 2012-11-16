3e323b1a58a931c073647328ac9a01a5ec307191
Oliver Kopp <olly98@users.sourceforge.net>
2012-11-16 23:27:15 +0100

    Adds support for java-swing-ayatana. refs feature #796
    Former-commit-id: 6ccc2c6d92d384ccaf37d5bbbe1752609c5abefb

--- src/java/net/sf/jabref/JabRefFrame.java
+++ src/java/net/sf/jabref/JabRefFrame.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  Copyright (C) 2003-2011 JabRef contributors.     This program is free software; you can redistribute it and/or modify     it under the terms of the GNU General Public License as published by     the Free Software Foundation; either version 2 of the License, or     (at your option) any later version.      This program is distributed in the hope that it will be useful,     but WITHOUT ANY WARRANTY; without even the implied warranty of     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     GNU General Public License for more details.      You should have received a copy of the GNU General Public License along     with this program; if not, write to the Free Software Foundation, Inc.,     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
+comment|/*  Copyright (C) 2003-2012 JabRef contributors.     This program is free software; you can redistribute it and/or modify     it under the terms of the GNU General Public License as published by     the Free Software Foundation; either version 2 of the License, or     (at your option) any later version.      This program is distributed in the hope that it will be useful,     but WITHOUT ANY WARRANTY; without even the implied warranty of     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     GNU General Public License for more details.      You should have received a copy of the GNU General Public License along     with this program; if not, write to the Free Software Foundation, Inc.,     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
 end_comment
 
 begin_package
@@ -261,6 +261,18 @@ operator|.
 name|event
 operator|.
 name|ChangeListener
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|java
+operator|.
+name|ayatana
+operator|.
+name|ApplicationMenu
 import|;
 end_import
 
@@ -3641,6 +3653,13 @@ expr_stmt|;
 block|}
 block|}
 block|}
+argument_list|)
+expr_stmt|;
+name|ApplicationMenu
+operator|.
+name|tryInstall
+argument_list|(
+name|this
 argument_list|)
 expr_stmt|;
 block|}
