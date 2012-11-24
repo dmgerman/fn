6db977075dde83ea759033636281a3a88fba18c6
Oliver Kopp <olly98@users.sourceforge.net>
2012-11-24 21:55:46 +0100

    Adds log output if FileUpdateMonitor was shut down.
    Former-commit-id: 173470a6306bc3b8785bdabd703c425e4b5c50e6

--- src/java/net/sf/jabref/collab/FileUpdateMonitor.java
+++ src/java/net/sf/jabref/collab/FileUpdateMonitor.java
@@ -80,6 +80,18 @@ name|Iterator
 import|;
 end_import
 
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
+name|logging
+operator|.
+name|Logger
+import|;
+end_import
+
 begin_comment
 comment|/**  * This thread monitors a set of files, each associated with a FileUpdateListener, for changes * in the file's last modification time stamp. The  */
 end_comment
@@ -92,6 +104,24 @@ name|FileUpdateMonitor
 extends|extends
 name|Thread
 block|{
+DECL|field|logger
+specifier|private
+specifier|static
+name|Logger
+name|logger
+init|=
+name|Logger
+operator|.
+name|getLogger
+argument_list|(
+name|FileUpdateMonitor
+operator|.
+name|class
+operator|.
+name|getName
+argument_list|()
+argument_list|)
+decl_stmt|;
 DECL|field|WAIT
 specifier|final
 name|int
@@ -244,6 +274,13 @@ name|InterruptedException
 name|ex
 parameter_list|)
 block|{
+name|logger
+operator|.
+name|finest
+argument_list|(
+literal|"FileUpdateMonitor has been interrupted."
+argument_list|)
+expr_stmt|;
 comment|/*  the (?) correct way to interrupt threads, according to     	   *  http://www.roseindia.net/javatutorials/shutting_down_threads_cleanly.shtml     	   */
 name|Thread
 operator|.
