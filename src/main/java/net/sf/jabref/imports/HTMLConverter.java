b8d7ab3fb0a9d9f2fe7abade1e4a60c87860ae86
Ruy Minoru Ito Takata <ruy.takata@gmail.com>
2015-03-31 15:43:22 -0300

    Fix for bug #1253: Cleanup entries error 2
    Committer: Ruy Minoru Ito Takata <ruy.takata@gmail.com>
    
    Former-commit-id: 65ddbbe725b6123ca676c312d492561edbb4a180

--- src/main/java/net/sf/jabref/imports/HTMLConverter.java
+++ src/main/java/net/sf/jabref/imports/HTMLConverter.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  Copyright (C) 2003-2012 JabRef contributors.     This program is free software; you can redistribute it and/or modify     it under the terms of the GNU General Public License as published by     the Free Software Foundation; either version 2 of the License, or     (at your option) any later version.      This program is distributed in the hope that it will be useful,     but WITHOUT ANY WARRANTY; without even the implied warranty of     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     GNU General Public License for more details.      You should have received a copy of the GNU General Public License along     with this program; if not, write to the Free Software Foundation, Inc.,     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
+comment|/*  Copyright (C) 2003-2015 JabRef contributors.     This program is free software; you can redistribute it and/or modify     it under the terms of the GNU General Public License as published by     the Free Software Foundation; either version 2 of the License, or     (at your option) any later version.      This program is distributed in the hope that it will be useful,     but WITHOUT ANY WARRANTY; without even the implied warranty of     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     GNU General Public License for more details.      You should have received a copy of the GNU General Public License along     with this program; if not, write to the Free Software Foundation, Inc.,     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
 end_comment
 
 begin_package
@@ -490,7 +490,7 @@ literal|"197"
 block|,
 literal|"Aring"
 block|,
-literal|"\\{\\\\AA\\}"
+literal|"\\{\\{\\\\AA\\}\\}"
 block|}
 block|,
 comment|// latin capital letter A with ring above
@@ -812,7 +812,7 @@ literal|"229"
 block|,
 literal|"aring"
 block|,
-literal|"\\{\\\\aa\\}"
+literal|"\\{\\{\\\\aa\\}\\}"
 block|}
 block|,
 comment|// latin small letter a with ring above
