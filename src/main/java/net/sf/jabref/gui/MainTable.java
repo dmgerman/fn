19270626d4c103389f30adad2405feace39a2aa4
Simon Harrer <simon.harrer@gmail.com>
2015-07-29 18:10:38 +0200

    Revert "Fix deprecation warnings of glazedlist library."
    This reverts commit 2620b964462b98c8cfa656d5dce72fb96618cb28 [formerly 0e9d56462581a533a5fe30b32932e2efd7af37ab].
    
    
    Former-commit-id: a4eac9f4c108fdac0e2c339c675be5802dc134f6

--- src/main/java/net/sf/jabref/gui/MainTable.java
+++ src/main/java/net/sf/jabref/gui/MainTable.java
@@ -302,7 +302,7 @@ name|glazedlists
 operator|.
 name|swing
 operator|.
-name|DefaultEventSelectionModel
+name|EventSelectionModel
 import|;
 end_import
 
@@ -316,7 +316,7 @@ name|glazedlists
 operator|.
 name|swing
 operator|.
-name|DefaultEventTableModel
+name|EventTableModel
 import|;
 end_import
 
@@ -426,7 +426,7 @@ decl_stmt|;
 DECL|field|selectionModel
 specifier|private
 specifier|final
-name|DefaultEventSelectionModel
+name|EventSelectionModel
 argument_list|<
 name|BibtexEntry
 argument_list|>
@@ -723,14 +723,14 @@ operator|=
 literal|null
 expr_stmt|;
 comment|//new HitOrMissComparator(groupMatcher);
-name|DefaultEventTableModel
+name|EventTableModel
 argument_list|<
 name|BibtexEntry
 argument_list|>
 name|tableModel
 init|=
 operator|new
-name|DefaultEventTableModel
+name|EventTableModel
 argument_list|<
 name|BibtexEntry
 argument_list|>
@@ -761,7 +761,7 @@ expr_stmt|;
 name|selectionModel
 operator|=
 operator|new
-name|DefaultEventSelectionModel
+name|EventSelectionModel
 argument_list|<
 name|BibtexEntry
 argument_list|>
