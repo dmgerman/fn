9f5ae7e8e89c96e6acf96e33921f2de3d34bcd49
Joerg Lenhard <joerg.lenhard@uni-bamberg.de>
2015-07-30 12:14:05 +0200

    Move TabLabelPattern to gui.preftabs
    Former-commit-id: 82a90701732d8b3fa669603a0fef45f0923e81e0

--- src/main/java/net/sf/jabref/gui/preftabs/TabLabelPattern.java
+++ src/main/java/net/sf/jabref/gui/preftabs/TabLabelPattern.java
@@ -4,13 +4,17 @@ comment|/*  Copyright (C) 2003-2012 JabRef contributors.     This program is fre
 end_comment
 
 begin_package
-DECL|package|net.sf.jabref
+DECL|package|net.sf.jabref.gui.preftabs
 package|package
 name|net
 operator|.
 name|sf
 operator|.
 name|jabref
+operator|.
+name|gui
+operator|.
+name|preftabs
 package|;
 end_package
 
@@ -105,6 +109,30 @@ operator|.
 name|event
 operator|.
 name|ChangeListener
+import|;
+end_import
+
+begin_import
+import|import
+name|net
+operator|.
+name|sf
+operator|.
+name|jabref
+operator|.
+name|Globals
+import|;
+end_import
+
+begin_import
+import|import
+name|net
+operator|.
+name|sf
+operator|.
+name|jabref
+operator|.
+name|JabRefPreferences
 import|;
 end_import
 
