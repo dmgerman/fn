cc0abe49f193863fd5fc22f303cec508a22887ce
Joerg Lenhard <joerg.lenhard@uni-bamberg.de>
2015-08-24 14:48:00 +0200

    Remove overwriting of unused BibtexEntryType method
    Former-commit-id: dbb67470351ec72de39db9c320c78c28cf7b42de

--- src/main/java/net/sf/jabref/model/entry/CustomEntryType.java
+++ src/main/java/net/sf/jabref/model/entry/CustomEntryType.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  Copyright (C) 2003-2011 JabRef contributors.     This program is free software; you can redistribute it and/or modify     it under the terms of the GNU General Public License as published by     the Free Software Foundation; either version 2 of the License, or     (at your option) any later version.      This program is distributed in the hope that it will be useful,     but WITHOUT ANY WARRANTY; without even the implied warranty of     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     GNU General Public License for more details.      You should have received a copy of the GNU General Public License along     with this program; if not, write to the Free Software Foundation, Inc.,     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
+comment|/*  Copyright (C) 2003-2015 JabRef contributors.     This program is free software; you can redistribute it and/or modify     it under the terms of the GNU General Public License as published by     the Free Software Foundation; either version 2 of the License, or     (at your option) any later version.      This program is distributed in the hope that it will be useful,     but WITHOUT ANY WARRANTY; without even the implied warranty of     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     GNU General Public License for more details.      You should have received a copy of the GNU General Public License along     with this program; if not, write to the Free Software Foundation, Inc.,     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
 end_comment
 
 begin_package
@@ -617,79 +617,6 @@ name|split
 argument_list|(
 literal|";"
 argument_list|)
-return|;
-block|}
-annotation|@
-name|Override
-DECL|method|describeRequiredFields ()
-specifier|public
-name|String
-name|describeRequiredFields
-parameter_list|()
-block|{
-name|StringBuilder
-name|result
-init|=
-operator|new
-name|StringBuilder
-argument_list|()
-decl_stmt|;
-for|for
-control|(
-name|int
-name|i
-init|=
-literal|0
-init|;
-name|i
-operator|<
-name|required
-operator|.
-name|length
-condition|;
-name|i
-operator|++
-control|)
-block|{
-name|result
-operator|.
-name|append
-argument_list|(
-name|required
-index|[
-name|i
-index|]
-argument_list|)
-expr_stmt|;
-name|result
-operator|.
-name|append
-argument_list|(
-name|i
-operator|<=
-name|required
-operator|.
-name|length
-operator|-
-literal|1
-operator|&&
-name|required
-operator|.
-name|length
-operator|>
-literal|1
-condition|?
-literal|", "
-else|:
-literal|""
-argument_list|)
-expr_stmt|;
-block|}
-return|return
-name|result
-operator|.
-name|toString
-argument_list|()
 return|;
 block|}
 comment|/**      * Check whether this entry's required fields are set, taking crossreferenced entries and      * either-or fields into account:      *      * @param entry    The entry to check.      * @param database The entry's database.      * @return True if required fields are set, false otherwise.      */
