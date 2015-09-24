cea3b62cf540f5b6e1f9457d79da7639ca2caf23
Tobias Denkinger <tobias.denkinger@gmx.de>
2015-09-24 15:49:52 +0200

    enabled AuxSubGenerator to parse biblatex generated AUX files
    Former-commit-id: 9d1f5bdc6f54dff467872a403a5f5c490312666e

--- src/main/java/net/sf/jabref/wizard/auximport/AuxSubGenerator.java
+++ src/main/java/net/sf/jabref/wizard/auximport/AuxSubGenerator.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/* Copyright (C) 2004 R. Nagel  All programs in this directory and subdirectories are published under the GNU General Public License as described below.  This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.  You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA  Further information about the GNU GPL is available at: http://www.gnu.org/copyleft/gpl.ja.html  */
+comment|/* Copyright (C) 2004 R. Nagel Copyright (C) 2015 T. Denkinger  All programs in this directory and subdirectories are published under the GNU General Public License as described below.  This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.  You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA  Further information about the GNU GPL is available at: http://www.gnu.org/copyleft/gpl.ja.html  */
 end_comment
 
 begin_comment
@@ -368,7 +368,7 @@ name|Pattern
 operator|.
 name|compile
 argument_list|(
-literal|"\\\\citation\\{.+\\}"
+literal|"\\\\(citation|abx@aux@cite)\\{(.+)\\}"
 argument_list|)
 expr_stmt|;
 comment|// input-file-buffer
@@ -615,23 +615,8 @@ init|=
 name|matcher
 operator|.
 name|group
-argument_list|()
-operator|.
-name|substring
 argument_list|(
-name|matcher
-operator|.
-name|start
-argument_list|()
-operator|+
-literal|10
-argument_list|,
-name|matcher
-operator|.
-name|end
-argument_list|()
-operator|-
-literal|1
+literal|2
 argument_list|)
 decl_stmt|;
 comment|// could be an comma separated list of keys
