f548cf80b88c268bebffaa4892b767481683d3fb
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2015-11-13 14:10:18 +0100

    Move keyword functions from Util to BibtexEntry and add addKeyword(s) methods
    Former-commit-id: 2dfdda522e6aff50caa1619e2868a1c176b0b332

--- src/main/java/net/sf/jabref/specialfields/SpecialFieldsUtils.java
+++ src/main/java/net/sf/jabref/specialfields/SpecialFieldsUtils.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  Copyright (C) 2012 JabRef contributors.     This program is free software; you can redistribute it and/or modify     it under the terms of the GNU General Public License as published by     the Free Software Foundation; either version 2 of the License, or     (at your option) any later version.      This program is distributed in the hope that it will be useful,     but WITHOUT ANY WARRANTY; without even the implied warranty of     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     GNU General Public License for more details.      You should have received a copy of the GNU General Public License along     with this program; if not, write to the Free Software Foundation, Inc.,     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
+comment|/*  Copyright (C) 2012-2015 JabRef contributors.     This program is free software; you can redistribute it and/or modify     it under the terms of the GNU General Public License as published by     the Free Software Foundation; either version 2 of the License, or     (at your option) any later version.      This program is distributed in the hope that it will be useful,     but WITHOUT ANY WARRANTY; without even the implied warranty of     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     GNU General Public License for more details.      You should have received a copy of the GNU General Public License along     with this program; if not, write to the Free Software Foundation, Inc.,     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
 end_comment
 
 begin_package
@@ -91,6 +91,22 @@ operator|.
 name|undo
 operator|.
 name|NamedCompound
+import|;
+end_import
+
+begin_import
+import|import
+name|net
+operator|.
+name|sf
+operator|.
+name|jabref
+operator|.
+name|gui
+operator|.
+name|undo
+operator|.
+name|UndoableFieldChange
 import|;
 end_import
 
@@ -479,12 +495,10 @@ name|String
 argument_list|>
 name|keywordList
 init|=
-name|Util
+name|be
 operator|.
 name|getSeparatedKeywords
-argument_list|(
-name|be
-argument_list|)
+argument_list|()
 decl_stmt|;
 name|List
 argument_list|<
@@ -578,19 +592,78 @@ argument_list|)
 expr_stmt|;
 block|}
 block|}
-name|Util
+name|String
+name|oldValue
+init|=
+name|be
+operator|.
+name|getField
+argument_list|(
+literal|"keywords"
+argument_list|)
+decl_stmt|;
+name|be
 operator|.
 name|putKeywords
 argument_list|(
+name|keywordList
+argument_list|)
+expr_stmt|;
+name|String
+name|updatedValue
+init|=
+name|be
+operator|.
+name|getField
+argument_list|(
+literal|"keywords"
+argument_list|)
+decl_stmt|;
+if|if
+condition|(
+operator|(
+name|oldValue
+operator|==
+literal|null
+operator|)
+operator|||
+operator|!
+name|oldValue
+operator|.
+name|equals
+argument_list|(
+name|updatedValue
+argument_list|)
+condition|)
+block|{
+if|if
+condition|(
+name|ce
+operator|!=
+literal|null
+condition|)
+block|{
+name|ce
+operator|.
+name|addEdit
+argument_list|(
+operator|new
+name|UndoableFieldChange
+argument_list|(
 name|be
 argument_list|,
-name|keywordList
+literal|"keywords"
 argument_list|,
-name|ce
+name|oldValue
+argument_list|,
+name|updatedValue
+argument_list|)
 argument_list|)
 expr_stmt|;
 block|}
-comment|/**      * Update keywords according to values of special fields      *       * @param nc indicates the undo named compound. May be null      */
+block|}
+block|}
+comment|/**      * Update keywords according to values of special fields      *      * @param nc indicates the undo named compound. May be null      */
 DECL|method|syncKeywordsFromSpecialFields (BibtexEntry be, NamedCompound nc)
 specifier|public
 specifier|static
@@ -769,7 +842,7 @@ name|nc
 argument_list|)
 expr_stmt|;
 block|}
-comment|/**      * updates field values according to keywords      *       * @param ce indicates the undo named compound. May be null      */
+comment|/**      * updates field values according to keywords      *      * @param ce indicates the undo named compound. May be null      */
 DECL|method|syncSpecialFieldsFromKeywords (BibtexEntry be, NamedCompound ce)
 specifier|public
 specifier|static
@@ -803,7 +876,17 @@ name|String
 argument_list|>
 name|keywordList
 init|=
-name|Util
+name|net
+operator|.
+name|sf
+operator|.
+name|jabref
+operator|.
+name|model
+operator|.
+name|entry
+operator|.
+name|EntryUtil
 operator|.
 name|getSeparatedKeywords
 argument_list|(
