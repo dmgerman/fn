fc1b6cd81104f70227d287f1d79ca736e877931e
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2015-11-13 10:38:56 +0100

    Sorted out the dependencies, now independent again
    Former-commit-id: 17f1113d0451cd415d7c74f09d95df7c08063d2d

--- src/main/java/net/sf/jabref/model/entry/YearUtil.java
+++ src/main/java/net/sf/jabref/model/entry/YearUtil.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_package
-DECL|package|net.sf.jabref.logic.util.date
+DECL|package|net.sf.jabref.model.entry
 package|package
 name|net
 operator|.
@@ -8,11 +8,9 @@ name|sf
 operator|.
 name|jabref
 operator|.
-name|logic
+name|model
 operator|.
-name|util
-operator|.
-name|date
+name|entry
 package|;
 end_package
 
