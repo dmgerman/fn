8758b38345b2030e4cfafbb331aaa1f38d20c1a2
Stefan Kolb <stefan-kolb@web.de>
2015-11-17 00:03:11 +0100

    Fix unicode
    Former-commit-id: 606cfc5ca7099faca056405c629b0f829bb6e5a8

--- src/main/java/net/sf/jabref/logic/integrity/IntegrityCheck.java
+++ src/main/java/net/sf/jabref/logic/integrity/IntegrityCheck.java
@@ -1004,7 +1004,7 @@ expr_stmt|;
 block|}
 block|}
 block|}
-comment|/**      * From BibTex manual:      * One or more page numbers or range of numbers, such as 42--111 or 7,41,73--97 or 43+      * (the '+' in this last example indicates pages following that dont form a simple range).      * To make it easier to maintain Scribe-compatible databases, the standard styles convert      * a single dash (as in 7-33) to the double dash used in TEX to denote number ranges (as in 7--33).      */
+comment|/**      * From BibTex manual:      * One or more page numbers or range of numbers, such as 42--111 or 7,41,73--97 or 43+      * (the '+' in this last example indicates pages following that don't form a simple range).      * To make it easier to maintain Scribe-compatible databases, the standard styles convert      * a single dash (as in 7-33) to the double dash used in TEX to denote number ranges (as in 7--33).      */
 DECL|class|PagesChecker
 specifier|private
 specifier|static
