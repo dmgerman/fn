7dcc68c35bc2a324d34f66fc51e07391e6cdfec9
matthiasgeiger <matthias.geiger@uni-bamberg.de>
2015-11-26 11:39:15 +0100

    localization cleanup in DroppedFileHandler
    Former-commit-id: 7e73a25ba23bb1a39e9a494dcaf0132000ce7fc6

--- src/main/java/net/sf/jabref/external/DroppedFileHandler.java
+++ src/main/java/net/sf/jabref/external/DroppedFileHandler.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  Copyright (C) 2003-2012 JabRef contributors.     This program is free software; you can redistribute it and/or modify     it under the terms of the GNU General Public License as published by     the Free Software Foundation; either version 2 of the License, or     (at your option) any later version.      This program is distributed in the hope that it will be useful,     but WITHOUT ANY WARRANTY; without even the implied warranty of     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     GNU General Public License for more details.      You should have received a copy of the GNU General Public License along     with this program; if not, write to the Free Software Foundation, Inc.,     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
+comment|/*  Copyright (C) 2003-2015 JabRef contributors.     This program is free software; you can redistribute it and/or modify     it under the terms of the GNU General Public License as published by     the Free Software Foundation; either version 2 of the License, or     (at your option) any later version.      This program is distributed in the hope that it will be useful,     but WITHOUT ANY WARRANTY; without even the implied warranty of     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     GNU General Public License for more details.      You should have received a copy of the GNU General Public License along     with this program; if not, write to the Free Software Foundation, Inc.,     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
 end_comment
 
 begin_package
@@ -2976,14 +2976,26 @@ name|Localization
 operator|.
 name|lang
 argument_list|(
-literal|"There was an error moving the file. Please move the file manually and link in place."
+literal|"Could not move file %0."
+argument_list|,
+name|toFile
+operator|.
+name|getAbsolutePath
+argument_list|()
+argument_list|)
+operator|+
+name|Localization
+operator|.
+name|lang
+argument_list|(
+literal|"Please move the file manually and link in place."
 argument_list|)
 argument_list|,
 name|Localization
 operator|.
 name|lang
 argument_list|(
-literal|"Error moving file"
+literal|"Move file failed"
 argument_list|)
 argument_list|,
 name|JOptionPane
