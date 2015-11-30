5e3acb5930a9f7e4804f6f6d9eea27a1dc18f800
matthiasgeiger <matthias.geiger@uni-bamberg.de>
2015-11-30 16:32:10 +0100

    correct usage of Charsets in GeneralTab - fixes #420
    Former-commit-id: 2d92f636b99484e3583c68f6b199e63f5f1792ba

--- src/main/java/net/sf/jabref/gui/preftabs/GeneralTab.java
+++ src/main/java/net/sf/jabref/gui/preftabs/GeneralTab.java
@@ -1931,12 +1931,12 @@ name|JabRefPreferences
 operator|.
 name|DEFAULT_ENCODING
 argument_list|,
-operator|(
-name|String
-operator|)
 name|encodings
 operator|.
 name|getSelectedItem
+argument_list|()
+operator|.
+name|toString
 argument_list|()
 argument_list|)
 expr_stmt|;
