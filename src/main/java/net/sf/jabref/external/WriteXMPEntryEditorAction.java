18a2048564d0b24a4b4e175730d2552ea921fbd7
Simon Harrer <simon.harrer@gmail.com>
2015-12-03 16:54:03 +0100

    do not use Localization.lang() in comment
    Former-commit-id: a5bb2ffb22c39b10740b940ff275bd65f253a3fa

--- src/main/java/net/sf/jabref/external/WriteXMPEntryEditorAction.java
+++ src/main/java/net/sf/jabref/external/WriteXMPEntryEditorAction.java
@@ -266,7 +266,7 @@ name|editor
 operator|=
 name|editor
 expr_stmt|;
-comment|// normally, the next call should be without "Localization.lang". However, the string "Write XMP" is also used in non-menu places and therefore, the translation must be also available at Localization.lang()
+comment|// normally, the next call should be without "Localization.lang". However, the string "Write XMP" is also used in non-menu places and therefore, the translation must be also available at Localization.lang
 name|putValue
 argument_list|(
 name|Action
