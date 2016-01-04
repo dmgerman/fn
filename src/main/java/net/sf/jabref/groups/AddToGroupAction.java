00e7daf46fa77d8ee401936a81168479301ac25a
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2016-01-04 18:49:31 +0100

    Replaced while-getNextElement-loops with foreach-loops
    Former-commit-id: 92d9a3a8c98c10e62a94526f0a63483ca7bfb7c9

--- src/main/java/net/sf/jabref/groups/AddToGroupAction.java
+++ src/main/java/net/sf/jabref/groups/AddToGroupAction.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  Copyright (C) 2003-2011 JabRef contributors.     This program is free software; you can redistribute it and/or modify     it under the terms of the GNU General Public License as published by     the Free Software Foundation; either version 2 of the License, or     (at your option) any later version.      This program is distributed in the hope that it will be useful,     but WITHOUT ANY WARRANTY; without even the implied warranty of     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     GNU General Public License for more details.      You should have received a copy of the GNU General Public License along     with this program; if not, write to the Free Software Foundation, Inc.,     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.  */
+comment|/*  Copyright (C) 2003-2015 JabRef contributors.     This program is free software; you can redistribute it and/or modify     it under the terms of the GNU General Public License as published by     the Free Software Foundation; either version 2 of the License, or     (at your option) any later version.      This program is distributed in the hope that it will be useful,     but WITHOUT ANY WARRANTY; without even the implied warranty of     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     GNU General Public License for more details.      You should have received a copy of the GNU General Public License along     with this program; if not, write to the Free Software Foundation, Inc.,     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.  */
 end_comment
 
 begin_package
@@ -25,6 +25,16 @@ operator|.
 name|event
 operator|.
 name|ActionEvent
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
+name|Collections
 import|;
 end_import
 
@@ -344,24 +354,19 @@ operator|.
 name|preorderEnumeration
 argument_list|()
 decl_stmt|;
+for|for
+control|(
 name|GroupTreeNode
 name|node
-decl_stmt|;
-while|while
-condition|(
-name|e
+range|:
+name|Collections
 operator|.
-name|hasMoreElements
-argument_list|()
-condition|)
+name|list
+argument_list|(
+name|e
+argument_list|)
+control|)
 block|{
-name|node
-operator|=
-name|e
-operator|.
-name|nextElement
-argument_list|()
-expr_stmt|;
 if|if
 condition|(
 operator|!
