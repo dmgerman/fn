cfbe2881708eb4d6ceabc211026c06416908db79
matthiasgeiger <matthias.geiger@uni-bamberg.de>
2016-01-27 14:50:47 +0100

    fixing saving without saveActions
    Former-commit-id: 01b4255c8f203f8eb1d675c1185c45fb88577293

--- src/main/java/net/sf/jabref/exporter/SaveActions.java
+++ src/main/java/net/sf/jabref/exporter/SaveActions.java
@@ -182,6 +182,9 @@ name|HashMap
 argument_list|<>
 argument_list|()
 expr_stmt|;
+name|setAvailableFormatters
+argument_list|()
+expr_stmt|;
 name|List
 argument_list|<
 name|String
@@ -195,9 +198,18 @@ argument_list|(
 name|META_KEY
 argument_list|)
 decl_stmt|;
-name|setAvailableFormatters
-argument_list|()
-expr_stmt|;
+if|if
+condition|(
+name|formatters
+operator|==
+literal|null
+condition|)
+block|{
+comment|// no save actions defined in the meta data
+return|return;
+block|}
+else|else
+block|{
 for|for
 control|(
 name|int
@@ -262,6 +274,7 @@ parameter_list|)
 block|{
 comment|// the meta data string in the file is broken. -> Ignore the last item
 break|break;
+block|}
 block|}
 block|}
 block|}
