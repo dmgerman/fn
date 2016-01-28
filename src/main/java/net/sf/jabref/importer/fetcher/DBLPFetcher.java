552f8bbb4567ee6ed971b63e8effa1215697d0a6
Oscar Gustafsson <ogib@users.sourceforge.net>
2016-01-28 19:20:32 +0100

    Always use URLDownload for fetching stuff
    Former-commit-id: 6f99d0fab87343760cfc2b22ef28d5b827a5e105

--- src/main/java/net/sf/jabref/importer/fetcher/DBLPFetcher.java
+++ src/main/java/net/sf/jabref/importer/fetcher/DBLPFetcher.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  Copyright (C) 2015 JabRef contributors.     Copyright (C) 2011 Sascha Hunold.     This program is free software; you can redistribute it and/or modify     it under the terms of the GNU General Public License as published by     the Free Software Foundation; either version 2 of the License, or     (at your option) any later version.      This program is distributed in the hope that it will be useful,     but WITHOUT ANY WARRANTY; without even the implied warranty of     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     GNU General Public License for more details.      You should have received a copy of the GNU General Public License along     with this program; if not, write to the Free Software Foundation, Inc.,     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
+comment|/*  Copyright (C) 2015-2016 JabRef contributors.     Copyright (C) 2011 Sascha Hunold.     This program is free software; you can redistribute it and/or modify     it under the terms of the GNU General Public License as published by     the Free Software Foundation; either version 2 of the License, or     (at your option) any later version.      This program is distributed in the hope that it will be useful,     but WITHOUT ANY WARRANTY; without even the implied warranty of     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     GNU General Public License for more details.      You should have received a copy of the GNU General Public License along     with this program; if not, write to the Free Software Foundation, Inc.,     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
 end_comment
 
 begin_package
@@ -158,7 +158,7 @@ name|logic
 operator|.
 name|net
 operator|.
-name|NetUtil
+name|URLDownload
 import|;
 end_import
 
@@ -351,15 +351,22 @@ argument_list|(
 name|address
 argument_list|)
 decl_stmt|;
-name|String
-name|page
+name|URLDownload
+name|dl
 init|=
-name|NetUtil
-operator|.
-name|getResults
+operator|new
+name|URLDownload
 argument_list|(
 name|url
 argument_list|)
+decl_stmt|;
+name|String
+name|page
+init|=
+name|dl
+operator|.
+name|downloadToString
+argument_list|()
 decl_stmt|;
 comment|//System.out.println(page);
 name|String
@@ -489,12 +496,14 @@ specifier|final
 name|String
 name|bibtexHTMLPage
 init|=
-name|NetUtil
-operator|.
-name|getResults
+operator|new
+name|URLDownload
 argument_list|(
 name|bibUrl
 argument_list|)
+operator|.
+name|downloadToString
+argument_list|()
 decl_stmt|;
 specifier|final
 name|String
@@ -547,7 +556,7 @@ argument_list|(
 literal|'}'
 argument_list|)
 decl_stmt|;
-comment|// now we take everything within the curley braces
+comment|// now we take everything within the curly braces
 name|String
 name|bibtexUrl
 init|=
@@ -589,12 +598,14 @@ specifier|final
 name|String
 name|bibtexPage
 init|=
-name|NetUtil
-operator|.
-name|getResults
+operator|new
+name|URLDownload
 argument_list|(
 name|bibFileURL
 argument_list|)
+operator|.
+name|downloadToString
+argument_list|()
 decl_stmt|;
 name|Collection
 argument_list|<
