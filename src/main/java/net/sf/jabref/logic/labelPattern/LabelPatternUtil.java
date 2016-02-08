dmgerman/fn

commit 2fdbb3e20c723f599141b29e82ba79ef4db5354e

Changes based on suggestions and further cleanup
Former-commit-id: 81f2a687010c4da31f1808133ae03fb3ef8cd0b9

src/main/java/net/sf/jabref/logic/labelPattern/LabelPatternUtil.java
@@ -1943,14 +1943,6 @@ argument_list|(
 name|entryType
 argument_list|)
 expr_stmt|;
-name|int
-name|typeListSize
-init|=
-name|typeList
-operator|.
-name|size
-argument_list|()
-decl_stmt|;
 name|boolean
 name|field
 init|=
@@ -1958,29 +1950,12 @@ literal|false
 decl_stmt|;
 for|for
 control|(
-name|int
-name|i
-init|=
-literal|1
-init|;
-name|i
-operator|<
-name|typeListSize
-condition|;
-name|i
-operator|++
-control|)
-block|{
 name|String
 name|typeListEntry
-init|=
+range|:
 name|typeList
-operator|.
-name|get
-argument_list|(
-name|i
-argument_list|)
-decl_stmt|;
+control|)
+block|{
 if|if
 condition|(
 literal|"["
@@ -4773,10 +4748,8 @@ if|if
 condition|(
 name|authorList
 operator|.
-name|size
+name|isEmpty
 argument_list|()
-operator|==
-literal|0
 condition|)
 block|{
 return|return
@@ -4830,10 +4803,8 @@ if|if
 condition|(
 name|authorList
 operator|.
-name|size
+name|isEmpty
 argument_list|()
-operator|==
-literal|0
 condition|)
 block|{
 return|return
@@ -4894,10 +4865,8 @@ if|if
 condition|(
 name|authorList
 operator|.
-name|size
+name|isEmpty
 argument_list|()
-operator|==
-literal|0
 condition|)
 block|{
 return|return
@@ -5072,10 +5041,8 @@ if|if
 condition|(
 name|authorList
 operator|.
-name|size
+name|isEmpty
 argument_list|()
-operator|==
-literal|0
 condition|)
 block|{
 return|return

src/main/java/net/sf/jabref/JabRef.java
@@ -174,34 +174,6 @@ name|jabref
 operator|.
 name|logic
 operator|.
-name|ProxyPreferences
-import|;
-end_import
-
-begin_import
-import|import
-name|net
-operator|.
-name|sf
-operator|.
-name|jabref
-operator|.
-name|logic
-operator|.
-name|ProxyRegisterer
-import|;
-end_import
-
-begin_import
-import|import
-name|net
-operator|.
-name|sf
-operator|.
-name|jabref
-operator|.
-name|logic
-operator|.
 name|journals
 operator|.
 name|Abbreviations
@@ -253,6 +225,38 @@ operator|.
 name|logging
 operator|.
 name|JabRefLogger
+import|;
+end_import
+
+begin_import
+import|import
+name|net
+operator|.
+name|sf
+operator|.
+name|jabref
+operator|.
+name|logic
+operator|.
+name|net
+operator|.
+name|ProxyPreferences
+import|;
+end_import
+
+begin_import
+import|import
+name|net
+operator|.
+name|sf
+operator|.
+name|jabref
+operator|.
+name|logic
+operator|.
+name|net
+operator|.
+name|ProxyRegisterer
 import|;
 end_import
 

src/main/java/net/sf/jabref/MetaData.java
@@ -1190,20 +1190,10 @@ argument_list|)
 expr_stmt|;
 for|for
 control|(
-name|int
-name|j
-init|=
-literal|0
-init|;
-name|j
-operator|<
+name|String
+name|data
+range|:
 name|orderedData
-operator|.
-name|size
-argument_list|()
-condition|;
-name|j
-operator|++
 control|)
 block|{
 name|sb
@@ -1214,12 +1204,7 @@ name|StringUtil
 operator|.
 name|quote
 argument_list|(
-name|orderedData
-operator|.
-name|get
-argument_list|(
-name|j
-argument_list|)
+name|data
 argument_list|,
 literal|";"
 argument_list|,

src/main/java/net/sf/jabref/gui/JabRefFrame.java
@@ -4392,7 +4392,7 @@ name|streamEavesdropper
 argument_list|,
 name|GuiAppender
 operator|.
-name|cache
+name|CACHE
 argument_list|)
 decl_stmt|;
 DECL|field|dbConnect

src/main/java/net/sf/jabref/gui/preftabs/NetworkTab.java
@@ -168,6 +168,24 @@ name|jabref
 operator|.
 name|logic
 operator|.
+name|l10n
+operator|.
+name|Localization
+import|;
+end_import
+
+begin_import
+import|import
+name|net
+operator|.
+name|sf
+operator|.
+name|jabref
+operator|.
+name|logic
+operator|.
+name|net
+operator|.
 name|ProxyPreferences
 import|;
 end_import
@@ -182,23 +200,9 @@ name|jabref
 operator|.
 name|logic
 operator|.
-name|ProxyRegisterer
-import|;
-end_import
-
-begin_import
-import|import
 name|net
 operator|.
-name|sf
-operator|.
-name|jabref
-operator|.
-name|logic
-operator|.
-name|l10n
-operator|.
-name|Localization
+name|ProxyRegisterer
 import|;
 end_import
 

src/main/java/net/sf/jabref/logic/logging/GuiAppender.java
@@ -16,22 +16,6 @@ end_package
 
 begin_import
 import|import
-name|net
-operator|.
-name|sf
-operator|.
-name|jabref
-operator|.
-name|logic
-operator|.
-name|logging
-operator|.
-name|Cache
-import|;
-end_import
-
-begin_import
-import|import
 name|org
 operator|.
 name|apache
@@ -231,12 +215,12 @@ name|GuiAppender
 extends|extends
 name|AbstractAppender
 block|{
-DECL|field|cache
+DECL|field|CACHE
 specifier|public
 specifier|static
 specifier|final
 name|Cache
-name|cache
+name|CACHE
 init|=
 operator|new
 name|Cache
@@ -363,7 +347,7 @@ name|LogEvent
 name|event
 parameter_list|)
 block|{
-name|cache
+name|CACHE
 operator|.
 name|add
 argument_list|(

src/main/java/net/sf/jabref/logic/net/ProxyPreferences.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_package
-DECL|package|net.sf.jabref.logic
+DECL|package|net.sf.jabref.logic.net
 package|package
 name|net
 operator|.
@@ -9,6 +9,8 @@ operator|.
 name|jabref
 operator|.
 name|logic
+operator|.
+name|net
 package|;
 end_package
 
@@ -32,31 +34,37 @@ name|ProxyPreferences
 block|{
 DECL|field|useProxy
 specifier|private
+specifier|final
 name|Boolean
 name|useProxy
 decl_stmt|;
 DECL|field|hostname
 specifier|private
+specifier|final
 name|String
 name|hostname
 decl_stmt|;
 DECL|field|port
 specifier|private
+specifier|final
 name|String
 name|port
 decl_stmt|;
 DECL|field|useAuthentication
 specifier|private
+specifier|final
 name|Boolean
 name|useAuthentication
 decl_stmt|;
 DECL|field|username
 specifier|private
+specifier|final
 name|String
 name|username
 decl_stmt|;
 DECL|field|password
 specifier|private
+specifier|final
 name|String
 name|password
 decl_stmt|;

src/main/java/net/sf/jabref/logic/net/ProxyRegisterer.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_package
-DECL|package|net.sf.jabref.logic
+DECL|package|net.sf.jabref.logic.net
 package|package
 name|net
 operator|.
@@ -9,6 +9,8 @@ operator|.
 name|jabref
 operator|.
 name|logic
+operator|.
+name|net
 package|;
 end_package
 

commit f3c2f8ee43b7925703aa8870485f873610289834

Fix a small bug in citekey generator
Generating a new key would prepend the pattern which resulted for
example in `[author]Weinstein` instead of the expected `Weinstein`.
The reason for the bug was that the list returned by
`getLabelPattern().getValue(entryType)` contains as the first item the
complete pattern.

This bug was introduced in
https://github.com/oscargus/jabref/commit/2fdbb3e20c723f599141b29e82ba79ef4db5354e [formerly 81f2a687010c4da31f1808133ae03fb3ef8cd0b9]

Former-commit-id: 758831d64b5d7ffbab12e12a1d72e0b2404bce4b

src/main/java/net/sf/jabref/logic/labelPattern/LabelPatternUtil.java
@@ -1893,12 +1893,6 @@ name|database
 operator|=
 name|dBase
 expr_stmt|;
-name|List
-argument_list|<
-name|String
-argument_list|>
-name|typeList
-decl_stmt|;
 name|String
 name|key
 decl_stmt|;
@@ -1931,8 +1925,16 @@ name|getType
 argument_list|()
 decl_stmt|;
 comment|// Get the arrayList corresponding to the type
+name|List
+argument_list|<
+name|String
+argument_list|>
 name|typeList
-operator|=
+init|=
+operator|new
+name|ArrayList
+argument_list|<>
+argument_list|(
 name|metaData
 operator|.
 name|getLabelPattern
@@ -1942,7 +1944,25 @@ name|getValue
 argument_list|(
 name|entryType
 argument_list|)
+argument_list|)
+decl_stmt|;
+if|if
+condition|(
+operator|!
+name|typeList
+operator|.
+name|isEmpty
+argument_list|()
+condition|)
+block|{
+name|typeList
+operator|.
+name|remove
+argument_list|(
+literal|0
+argument_list|)
 expr_stmt|;
+block|}
 name|boolean
 name|field
 init|=