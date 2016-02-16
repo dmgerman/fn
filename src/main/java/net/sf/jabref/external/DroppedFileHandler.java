33acbb4f83a3d7663481daf363389384d261e375
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2016-02-16 09:45:20 +0100

    Added log message
    Former-commit-id: 15939da116a86d594fec5ac32d6ab51144d57770

--- src/main/java/net/sf/jabref/external/DroppedFileHandler.java
+++ src/main/java/net/sf/jabref/external/DroppedFileHandler.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  Copyright (C) 2003-2015 JabRef contributors.     This program is free software; you can redistribute it and/or modify     it under the terms of the GNU General Public License as published by     the Free Software Foundation; either version 2 of the License, or     (at your option) any later version.      This program is distributed in the hope that it will be useful,     but WITHOUT ANY WARRANTY; without even the implied warranty of     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     GNU General Public License for more details.      You should have received a copy of the GNU General Public License along     with this program; if not, write to the Free Software Foundation, Inc.,     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
+comment|/*  Copyright (C) 2003-2016 JabRef contributors.     This program is free software; you can redistribute it and/or modify     it under the terms of the GNU General Public License as published by     the Free Software Foundation; either version 2 of the License, or     (at your option) any later version.      This program is distributed in the hope that it will be useful,     but WITHOUT ANY WARRANTY; without even the implied warranty of     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     GNU General Public License for more details.      You should have received a copy of the GNU General Public License along     with this program; if not, write to the Free Software Foundation, Inc.,     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
 end_comment
 
 begin_package
@@ -1931,7 +1931,7 @@ literal|true
 return|;
 block|}
 comment|//
-comment|// @return true if user pushed "Ok", false otherwise
+comment|// @return true if user pushed "OK", false otherwise
 comment|//
 DECL|method|showLinkMoveCopyRenameDialog (String linkFileName, ExternalFileType fileType, BibEntry entry, boolean newEntry, final boolean multipleEntries, BibDatabase database)
 specifier|private
@@ -2958,6 +2958,13 @@ block|{
 comment|// OOps, we don't know which directory to put it in, or the given
 comment|// dir doesn't exist....
 comment|// This should not happen!!
+name|LOGGER
+operator|.
+name|warn
+argument_list|(
+literal|"Cannot determine destination directory or destination directory does not exist"
+argument_list|)
+expr_stmt|;
 return|return
 literal|false
 return|;
@@ -3202,6 +3209,13 @@ block|{
 comment|// OOps, we don't know which directory to put it in, or the given
 comment|// dir doesn't exist....
 comment|// This should not happen!!
+name|LOGGER
+operator|.
+name|warn
+argument_list|(
+literal|"Cannot determine destination directory or destination directory does not exist"
+argument_list|)
+expr_stmt|;
 return|return
 literal|false
 return|;
