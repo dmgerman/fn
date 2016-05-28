5f47eb28c0b1b4c11a87d056e548385feb4182ee
Simon Harrer <simon.harrer@gmail.com>
2016-05-28 16:20:56 +0200

    Remove unused imports
    Former-commit-id: 886136dc6b252dd62d36fa1fe895ee7746e56dbc

--- src/main/java/net/sf/jabref/gui/BasePanel.java
+++ src/main/java/net/sf/jabref/gui/BasePanel.java
@@ -270,16 +270,6 @@ name|java
 operator|.
 name|util
 operator|.
-name|Timer
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
 name|TimerTask
 import|;
 end_import
