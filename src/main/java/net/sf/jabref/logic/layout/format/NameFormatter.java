e0900d0b97088e94a27b950df16c61759dacdc08
Oliver Kopp <kopp.dev@gmail.com>
2016-12-02 21:59:12 +0100

    Add Objects.requireNonNull to track down test issue on travis
    Former-commit-id: eb723f313862b4d804be80c1cd96b69da1caa825

--- src/main/java/net/sf/jabref/logic/layout/format/NameFormatter.java
+++ src/main/java/net/sf/jabref/logic/layout/format/NameFormatter.java
@@ -48,6 +48,16 @@ end_import
 
 begin_import
 import|import
+name|java
+operator|.
+name|util
+operator|.
+name|Objects
+import|;
+end_import
+
+begin_import
+import|import
 name|net
 operator|.
 name|sf
@@ -609,6 +619,13 @@ name|NameFormatterPreferences
 name|prefs
 parameter_list|)
 block|{
+name|Objects
+operator|.
+name|requireNonNull
+argument_list|(
+name|prefs
+argument_list|)
+expr_stmt|;
 name|Map
 argument_list|<
 name|String
