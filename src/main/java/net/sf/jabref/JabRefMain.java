1c64df4e33d5b96e17125521201f1dd2a58b542d
matthiasgeiger <matthias.geiger@uni-bamberg.de>
2016-12-13 11:09:16 +0100

    migrate customEntryType preferences
    Former-commit-id: 9678cbad3475eae56a4c3f5983c5e424e998878a

--- src/main/java/net/sf/jabref/JabRefMain.java
+++ src/main/java/net/sf/jabref/JabRefMain.java
@@ -545,6 +545,11 @@ operator|.
 name|upgradeLabelPatternToBibtexKeyPattern
 argument_list|()
 expr_stmt|;
+name|PreferencesMigrations
+operator|.
+name|upgradeStoredCustomEntryTypes
+argument_list|()
+expr_stmt|;
 comment|// Update handling of special fields based on preferences
 name|InternalBibtexFields
 operator|.
