c64fd20d10321d344aa4ac633f1d352e134fb780
Joerg Lenhard <joerg.lenhard@kau.se>
2017-09-08 10:57:42 +0200

    Remove unused import
    Former-commit-id: 4a6bf01d66ca4fe703e964204b19adeb90cab102

--- src/main/java/org/jabref/gui/groups/GroupTreeController.java
+++ src/main/java/org/jabref/gui/groups/GroupTreeController.java
@@ -80,18 +80,6 @@ end_import
 
 begin_import
 import|import
-name|java
-operator|.
-name|util
-operator|.
-name|stream
-operator|.
-name|Collectors
-import|;
-end_import
-
-begin_import
-import|import
 name|javax
 operator|.
 name|inject
