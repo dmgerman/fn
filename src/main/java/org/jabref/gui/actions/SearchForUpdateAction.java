b6d5c68c03adff82c01052d625f563efb7c488b6
Siedlerchr <siedlerkiller@gmail.com>
2018-02-11 00:15:27 +0100

    convert help menu
    Former-commit-id: d3cd8aa4835ca803f3d74393b0d1d0a1ab53ca5a

--- src/main/java/org/jabref/gui/actions/SearchForUpdateAction.java
+++ src/main/java/org/jabref/gui/actions/SearchForUpdateAction.java
@@ -14,47 +14,11 @@ end_package
 
 begin_import
 import|import
-name|java
-operator|.
-name|awt
-operator|.
-name|event
-operator|.
-name|ActionEvent
-import|;
-end_import
-
-begin_import
-import|import
-name|javax
-operator|.
-name|swing
-operator|.
-name|AbstractAction
-import|;
-end_import
-
-begin_import
-import|import
 name|org
 operator|.
 name|jabref
 operator|.
 name|JabRefGUI
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|jabref
-operator|.
-name|logic
-operator|.
-name|l10n
-operator|.
-name|Localization
 import|;
 end_import
 
@@ -64,34 +28,15 @@ specifier|public
 class|class
 name|SearchForUpdateAction
 extends|extends
-name|AbstractAction
+name|SimpleCommand
 block|{
-DECL|method|SearchForUpdateAction ()
-specifier|public
-name|SearchForUpdateAction
-parameter_list|()
-block|{
-name|super
-argument_list|(
-name|Localization
-operator|.
-name|lang
-argument_list|(
-literal|"Check for updates"
-argument_list|)
-argument_list|)
-expr_stmt|;
-block|}
 annotation|@
 name|Override
-DECL|method|actionPerformed (ActionEvent e)
+DECL|method|execute ()
 specifier|public
 name|void
-name|actionPerformed
-parameter_list|(
-name|ActionEvent
-name|e
-parameter_list|)
+name|execute
+parameter_list|()
 block|{
 name|JabRefGUI
 operator|.
