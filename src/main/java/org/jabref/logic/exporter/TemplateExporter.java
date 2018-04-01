edb8077ce0f329007eaf55953bef0ea440acc238
Oliver Kopp <kopp.dev@gmail.com>
2018-04-01 12:20:31 +0200

    Remove obsolete comments
    Former-commit-id: 44fc1556f81e0030bd621bcfc8fba748112cf582

--- src/main/java/org/jabref/logic/exporter/TemplateExporter.java
+++ src/main/java/org/jabref/logic/exporter/TemplateExporter.java
@@ -605,7 +605,7 @@ return|return
 name|this
 return|;
 block|}
-comment|/**      * This method should return a reader from which the given layout file can      * be read.      *<p>      *<p>      * Subclasses of TemplateExporter are free to override and provide their own      * implementation.      *      * @param filename the filename      * @return a newly created reader      * @throws IOException if the reader could not be created      */
+comment|/**      * This method should return a reader from which the given layout file can      * be read.      *<p>      * Subclasses of TemplateExporter are free to override and provide their own      * implementation.      *      * @param filename the filename      * @return a newly created reader      * @throws IOException if the reader could not be created      */
 DECL|method|getReader (String filename)
 specifier|private
 name|Reader
