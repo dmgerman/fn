1dffb53e5930ffa0f0b06bb6c3c9f25a694d9708
dpolishc <dpolishc@cisco.com>
2019-05-20 07:56:43 +0200

    Duplicate check on import should be run in background Task  #4963 (adding background task for view model)
    Former-commit-id: 6ec9ee8ed83080d71639426b8fe7da8783e6425f

--- src/main/java/org/jabref/gui/importer/ImportEntriesDialog.java
+++ src/main/java/org/jabref/gui/importer/ImportEntriesDialog.java
@@ -24,18 +24,6 @@ end_import
 
 begin_import
 import|import
-name|java
-operator|.
-name|util
-operator|.
-name|concurrent
-operator|.
-name|Callable
-import|;
-end_import
-
-begin_import
-import|import
 name|javax
 operator|.
 name|inject
