4f6f1f6e8976d29695017028e1cac91169a5cd3f
Tobias Diez <tobiasdiez@gmx.de>
2019-09-26 13:18:41 +0200

    Bring back the context menu (#5352)
    As explained in https://github.com/JabRef/jabref/issues/5254#issuecomment-526588221 it is no longer possible to customize the context menu items, because the classes related to the text field behavior are not accessible. Thus, instead I simply copied the relevant code from openjdk. Pretty ugly workaround... Fixes #5254 and refs https://github.com/javafxports/openjdk-jfx/issues/583.
    
    Moreover, I removed the custom tab handling because it is now working as expected (i.e. #2902 is fixed in Java 9+).
    
    Former-commit-id: d1307a8bbf5471f0d061e6e66222b42aff62de53

--- src/main/java/org/jabref/gui/fieldeditors/EditorTextArea.java
+++ src/main/java/org/jabref/gui/fieldeditors/EditorTextArea.java
@@ -82,7 +82,7 @@ name|scene
 operator|.
 name|control
 operator|.
-name|MenuItem
+name|ContextMenu
 import|;
 end_import
 
@@ -94,39 +94,9 @@ name|scene
 operator|.
 name|control
 operator|.
-name|skin
-operator|.
-name|TextAreaSkin
+name|MenuItem
 import|;
 end_import
-
-begin_import
-import|import
-name|javafx
-operator|.
-name|scene
-operator|.
-name|input
-operator|.
-name|KeyCode
-import|;
-end_import
-
-begin_import
-import|import
-name|javafx
-operator|.
-name|scene
-operator|.
-name|input
-operator|.
-name|KeyEvent
-import|;
-end_import
-
-begin_comment
-comment|// TODO: TextAreaSkin changed in Java 9
-end_comment
 
 begin_class
 DECL|class|EditorTextArea
@@ -146,7 +116,17 @@ name|Initializable
 implements|,
 name|ContextMenuAddable
 block|{
-comment|/**      *  Variable that contains user-defined behavior for paste action.      */
+DECL|field|contextMenu
+specifier|private
+specifier|final
+name|ContextMenu
+name|contextMenu
+init|=
+operator|new
+name|ContextMenu
+argument_list|()
+decl_stmt|;
+comment|/**      * Variable that contains user-defined behavior for paste action.      */
 DECL|field|pasteActionHandler
 specifier|private
 name|PasteActionHandler
@@ -189,51 +169,6 @@ argument_list|(
 literal|true
 argument_list|)
 expr_stmt|;
-comment|// Should behave as a normal text field with respect to TAB behaviour
-name|addEventFilter
-argument_list|(
-name|KeyEvent
-operator|.
-name|KEY_PRESSED
-argument_list|,
-name|event
-lambda|->
-block|{
-if|if
-condition|(
-name|event
-operator|.
-name|getCode
-argument_list|()
-operator|==
-name|KeyCode
-operator|.
-name|TAB
-condition|)
-block|{
-comment|// TODO: temporarily removed, as this is internal API
-comment|//                TextAreaSkin skin = (TextAreaSkin) getSkin();
-comment|//                if (event.isShiftDown()) {
-comment|//                    // Shift + Tab> previous text area
-comment|//                    skin.getBehavior().traversePrevious();
-comment|//                } else {
-comment|//                    if (event.isControlDown()) {
-comment|//                        // Ctrl + Tab> insert tab
-comment|//                        skin.getBehavior().callAction("InsertTab");
-comment|//                    } else {
-comment|//                        // Tab> next text area
-comment|//                        skin.getBehavior().traverseNext();
-comment|//                    }
-comment|//                }
-name|event
-operator|.
-name|consume
-argument_list|()
-expr_stmt|;
-block|}
-block|}
-argument_list|)
-expr_stmt|;
 block|}
 annotation|@
 name|Override
@@ -253,26 +188,53 @@ argument_list|>
 name|items
 parameter_list|)
 block|{
-name|TextAreaSkin
-name|customContextSkin
-init|=
-operator|new
-name|TextAreaSkin
+name|setOnContextMenuRequested
+argument_list|(
+name|event
+lambda|->
+block|{
+name|contextMenu
+operator|.
+name|getItems
+argument_list|()
+operator|.
+name|setAll
+argument_list|(
+name|TextInputControlBehavior
+operator|.
+name|getDefaultContextMenuItems
 argument_list|(
 name|this
 argument_list|)
-block|{
-comment|// TODO: temporarily removed, internal API
-comment|//            @Override
-comment|//            public void populateContextMenu(ContextMenu contextMenu) {
-comment|//                super.populateContextMenu(contextMenu);
-comment|//                contextMenu.getItems().addAll(0, items.get());
-comment|//            }
-block|}
-decl_stmt|;
-name|setSkin
+argument_list|)
+expr_stmt|;
+name|contextMenu
+operator|.
+name|getItems
+argument_list|()
+operator|.
+name|addAll
 argument_list|(
-name|customContextSkin
+literal|0
+argument_list|,
+name|items
+operator|.
+name|get
+argument_list|()
+argument_list|)
+expr_stmt|;
+name|TextInputControlBehavior
+operator|.
+name|showContextMenu
+argument_list|(
+name|this
+argument_list|,
+name|contextMenu
+argument_list|,
+name|event
+argument_list|)
+expr_stmt|;
+block|}
 argument_list|)
 expr_stmt|;
 block|}
@@ -292,7 +254,7 @@ parameter_list|)
 block|{
 comment|// not needed
 block|}
-comment|/**      * Set pasteActionHandler variable to passed handler      * @param  handler an instance of PasteActionHandler that describes paste behavior      */
+comment|/**      * Set pasteActionHandler variable to passed handler      *      * @param handler an instance of PasteActionHandler that describes paste behavior      */
 DECL|method|setPasteActionHandler (PasteActionHandler handler)
 specifier|public
 name|void
@@ -316,7 +278,7 @@ operator|=
 name|handler
 expr_stmt|;
 block|}
-comment|/**      *  Override javafx TextArea method applying TextArea.paste() and pasteActionHandler after      */
+comment|/**      * Override javafx TextArea method applying TextArea.paste() and pasteActionHandler after      */
 annotation|@
 name|Override
 DECL|method|paste ()
@@ -336,7 +298,7 @@ name|handle
 argument_list|()
 expr_stmt|;
 block|}
-comment|/**      *  Interface presents user-described paste behaviour applying to paste method      */
+comment|/**      * Interface presents user-described paste behaviour applying to paste method      */
 annotation|@
 name|FunctionalInterface
 DECL|interface|PasteActionHandler
